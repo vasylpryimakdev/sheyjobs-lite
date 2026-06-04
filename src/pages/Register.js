d4ef580cb60530b09b044817a7865681e081e4a3
Vasyl Pryimak <vasyl.pryimak.dev@gmail.com>
2026-06-04 20:02:49 +0300

    feat(auth): add register page UI with Ant Design form and navigation links

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,7 +1,33 @@
 import React from "react";
+import { Form } from "antd";
+import { Link } from "react-router-dom";
 
 function Register() {
-  return <div>Register</div>;
+  return (
+    <div className="h-screen d-flex justify-content-center align-items-center bg-primary">
+      <div className="bg-white p-4 w-400">
+        <h4>SHEYJOBS - REGISTER</h4>
+        <div className="divider"></div>
+        <Form layout="vertical">
+          <Form.Item name="name" label="Name">
+            <input type="text" />
+          </Form.Item>
+          <Form.Item name="email" label="Email">
+            <input type="text" />
+          </Form.Item>
+          <Form.Item name="password" label="Password">
+            <input type="password" />
+          </Form.Item>
+          <button className="primary-contained-btn w-100 mt-2" type="submit">
+            Register
+          </button>
+          <Link to="/login" className="d-block mt-2">
+            Already a member? Click here to login
+          </Link>
+        </Form>
+      </div>
+    </div>
+  );
 }
 
 export default Register;
