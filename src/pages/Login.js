d0b318b394d92d24e916aa8578dad5b7ed633935
Vasyl Pryimak <vasyl.pryimak.dev@gmail.com>
2026-06-04 20:02:16 +0300

    feat(auth): implement login page UI with Ant Design form and base styling system

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,30 @@
 import React from "react";
+import { Form } from "antd";
+import { Link } from "react-router-dom";
 
 function Login() {
-  return <div>Login</div>;
+  return (
+    <div className="h-screen d-flex justify-content-center align-items-center bg-primary">
+      <div className="bg-white p-4 w-400">
+        <h4>SHEYJOBS - LOGIN</h4>
+        <div className="divider"></div>
+        <Form layout="vertical">
+          <Form.Item name="email" label="Email">
+            <input type="text" />
+          </Form.Item>
+          <Form.Item name="password" label="Password">
+            <input type="password" />
+          </Form.Item>
+          <button className="primary-contained-btn w-100 mt-2" type="submit">
+            Login
+          </button>
+          <Link to="/register" className="d-block mt-2">
+            Not a member? Click here to register
+          </Link>
+        </Form>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
