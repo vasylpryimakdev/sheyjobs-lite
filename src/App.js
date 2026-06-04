b197d597fc09b5a2fca0d1a581df8f26caa7207b
Vasyl Pryimak <vasyl.pryimak.dev@gmail.com>
2026-06-04 20:01:30 +0300

    add authentication routes and pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import "./App.css";
-import { Button } from "antd";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
     <div>
-      <h1>SHEYJOBS - LITE</h1>
-      <Button type="primary">Primary Button</Button>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
